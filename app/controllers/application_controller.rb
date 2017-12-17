015ca6ae4ce6102afdc4df864769b5dc7932b927
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-17 00:20:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
-    post '/' do
+    post '/teams' do
       @team = Team.new(params[:team])
       params[:team][:heroes].each do |hero|
         SuperHero.new(hero)
